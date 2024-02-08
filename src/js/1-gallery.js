ae0c580c9594c212b2a3b23c643de9f5a30f6d56
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2024-02-08 18:20:53 +0100

    исвтр

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -20,25 +20,3 @@ new SimpleLightbox('.gallery a', {
   captionDelay: 250,
   captionsData: 'alt',
 });
-const gallery = document.querySelector('.gallery');
-
-function renderGallery(images) {
-  const markup = images
-    .map(
-      el => `<li class="gallery-item">
-                <a class="gallery-link" href="${el.original}">
-                  <img class="gallery-image" src="${el.preview}" alt="${el.description}" />
-                </a>
-              </li>`
-    )
-    .join('\n');
-
-  gallery.insertAdjacentHTML('afterbegin', markup);
-}
-
-renderGallery(images);
-
-new SimpleLightbox('.gallery a', {
-  captionDelay: 250,
-  captionsData: 'alt',
-});
