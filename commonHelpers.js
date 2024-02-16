b4113de9fb684f0d76778e63575f86c3b4a2e833
ukrainian-bird1984 <143355151+ukrainian-bird1984@users.noreply.github.com>
2024-02-16 19:19:39 +0000

    Deploying to gh-pages from @ ukrainian-bird1984/goit-js-hw-09@0ff9783eacea2bc1eb032785f30931c423e83949 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,7 +1,7 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";const i=document.querySelector(".gallery");function r(e){const l=e.map(a=>`<li class="gallery-item">
+import"./assets/modulepreload-polyfill-3cfb730f.js";const r=document.querySelector(".gallery");function i(e){const l=e.map(a=>`<li class="gallery-item">
                 <a class="gallery-link" href="${a.original}">
                   <img class="gallery-image" src="${a.preview}" alt="${a.description}" />
                 </a>
               </li>`).join(`
-`);i.insertAdjacentHTML("afterbegin",l)}r(images);new SimpleLightbox(".gallery a",{captionDelay:250,captionsData:"alt"});
+`);r.insertAdjacentHTML("afterbegin",l)}i(markup);new SimpleLightbox(".gallery a",{captionDelay:250,captionsData:"alt"});
 //# sourceMappingURL=commonHelpers.js.map
